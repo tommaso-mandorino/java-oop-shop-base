fc0be9a363d704a4a1c48aa938debc88dec8a4a8
tommaso-mandorino <tommasomandorino@outlook.it>
2026-01-11 01:51:41 +0100

    feat: add "Main" class to package

---
+++ src/org/lessons/java/shop/Main.java
@@ -0,0 +1,13 @@
+package org.lessons.java.shop;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        Prodotto prodotto1 = new Prodotto();
+
+        System.out.println(prodotto1.codice);
+
+    }
+
+}
