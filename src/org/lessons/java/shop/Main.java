d07eff5d90c2a5a8cda7dcebcbd68cad3dba403c
tommaso-mandorino <tommasomandorino@outlook.it>
2026-01-14 04:39:51 +0100

    feat: add new constructor with no parameters required, add brand attribute, add getters and setters for all attributes

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -6,10 +6,17 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Product product1 = new Product("TestProduct", "TestProduct Description", new BigDecimal(5.50), new BigDecimal(22));
+        Product product1 = new Product("TestName", "TestBrand", "TestDescription", new BigDecimal(5.50), new BigDecimal(22));
+        Product product2 = new Product();
+
+        System.out.println("");
 
         System.out.println(product1.getCode());
+        System.out.println(product1.getName());
+        System.out.println(product1.getBrand());
+        System.out.println(product1.getDescription());
         System.out.println(product1.getBasePrice());
+        System.out.println(product1.getVat());
         System.out.println(product1.getFinalPrice());
         System.out.println(product1.getExtendedName());
     }
--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -8,18 +8,52 @@ public class Product {
 
     int code;
     String name;
+    String brand;
     String description;
     BigDecimal price;
     BigDecimal vat;
 
-    Product(String name, String description, BigDecimal price, BigDecimal vat) {
+    Product() {
+        System.out.println("This is the test of the constructor with no parameters.");
+    }
+
+    Product(String name, String brand, String description, BigDecimal price, BigDecimal vat) {
 
         Random random = new Random();
         this.code = random.nextInt(5001) + 1;
 
         this.name = name;
+        this.brand = brand;
         this.description = description;
         this.price = price;
+        this.vat = vat;
+    }
+
+    String getName() {
+        return this.name;
+    }
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getBrand() {
+        return this.brand;
+    }
+    void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    String getDescription() {
+        return this.description;
+    }
+    void setDescription(String description) {
+        this.description = description;
+    }
+
+    BigDecimal getVat() {
+        return this.vat;
+    }
+    void setVat(BigDecimal vat) {
         this.vat = vat;
     }
 
